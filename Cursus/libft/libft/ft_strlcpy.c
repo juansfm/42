6d3d48f714d5dff24635cc736ed736a736285302
juansfm <114689723+juansfm@users.noreply.github.com>
2022-10-01 16:46:48 +0200

    Add files via upload

---
+++ Cursus/libft/libft/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsaavedr <jsaavedr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 19:28:57 by jsaavedr          #+#    #+#             */
+/*   Updated: 2022/09/25 19:49:19 by jsaavedr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	k;
+
+	k = ft_strlen(src);
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (i < dstsize - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (k);
+}
