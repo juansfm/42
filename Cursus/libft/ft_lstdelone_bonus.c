c950085888b307f253f6165aa28500fc7e74a0fe
Juan <juansfme@gmail.com>
2022-10-08 20:58:45 +0200

    08/10/2022

--- Cursus/libft/ft_lstdelone_bonus.c
+++ Cursus/libft/ft_lstdelone_bonus.c
@@ -6,12 +6,17 @@
 /*   By: jsaavedr <jsaavedr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:13:18 by jsaavedr          #+#    #+#             */
-/*   Updated: 2022/10/08 19:28:08 by jsaavedr         ###   ########.fr       */
+/*   Updated: 2022/10/08 20:52:11 by jsaavedr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_lstdelone(t_list *lst, void (*del)(void *))
-// {
-// }
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst)
+		return ;
+	del(lst->content);
+	free(lst->content);
+	lst = lst->next;
+}
