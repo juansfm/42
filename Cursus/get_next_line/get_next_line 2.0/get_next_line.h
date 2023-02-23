635081f5c122ea2a9a1039f1afc8e585c2c95a69
Juan <juansfme@gmail.com>
2023-02-23 12:36:31 +0100

    Versión final de get_next_line v2

--- Cursus/get_next_line/get_next_line 2.0/get_next_line.h
+++ Cursus/get_next_line/get_next_line 2.0/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: jsaavedr <jsaavedr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:28:09 by jsaavedr          #+#    #+#             */
-/*   Updated: 2023/02/11 18:45:12 by jsaavedr         ###   ########.fr       */
+/*   Updated: 2023/02/23 11:30:24 by jsaavedr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 500000
+#  define BUFFER_SIZE 1
 # endif
 
 # include <fcntl.h>
@@ -22,11 +22,11 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_line(char *line, char *buffer);
+char	*ft_line(char *buffer);
 char	*ft_buffer(char *buffer);
+char	*ft_read(int fd, char *buffer);
 size_t	ft_strlen(const char *s);
-int		ft_strchr(char *s, int c);
+size_t	ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
